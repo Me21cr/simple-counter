faad99e1b2d735f32512a7aa2b44a99e04e997a0
Me21cr <128864515+Me21cr@users.noreply.github.com>
2023-08-13 18:24:47 +0000

    cambios

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,18 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
+
+let segundo= 0;
+setInterval( ()=> {
+    const digitosix= Math.floor ( (segundo/100000 )%10); 
+    const digitofive= Math.floor ( (segundo/10000 )%10); 
+    const digitofour= Math.floor ( (segundo/1000 )%10); 
+    const digitothree= Math.floor ( (segundo/100 )%10); 
+    const digitotwo= Math.floor ( (segundo/10 )%10); 
+const digitoone= Math.floor ( (segundo/1 )%10);
+segundo +=1;
+
+//render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Home digito1={digitoone} digito2={digitotwo} digito3={digitothree} digito4={digitofour} digito5={digitofive} digito6={digitosix}/>, document.querySelector("#app"));
+}, 1000)
