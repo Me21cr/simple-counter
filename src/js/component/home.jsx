53f14eba1faa1fc9a5ff455b7a3a749e3ff5c9c6
Me21cr <128864515+Me21cr@users.noreply.github.com>
2023-08-16 21:00:16 +0000

    final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,16 +7,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = (props) => {
 	return (
 		<div className='container contador'>
-                 <div className='row contador xl '>
-                 <div className='col rounded text-white text-center display-5 border-start border-end digito'><i class="fa-solid fa-clock" style="color: #ffffff;"></i></div>
-				 <div className='col rounded text-white text-center display-5 border-start border-end digito'> {props.digitosix}</div>
-				 <div className='col rounded text-white text-center display-5 border-start border-end digito'>{props.digitofive}</div>
-				 <div className='col rounded text-white text-center display-5 border-start border-end digito'> {props.digitofour}</div>
-				 <div className='col rounded text-white text-center display-5 border-start border-end digito'>{props.digitothree}</div>
-				 <div className='col rounded text-white text-center display-5 border-start border-end digito'> {props.digitotwo}</div>
-				 <div className='col rounded text-white text-center display-5 border-start border-end digito'> {props.digitoone}</div>
-			
-		</div>
+			<div className='row contador xl '>
+				<div className='col rounded text-white text-center display-5 border-start border-end digito'><i className="far fa-clock"></i></div>
+				<div className='col rounded text-white text-center display-5 border-start border-end digito'> {props.digito6}</div>
+				<div className='col rounded text-white text-center display-5 border-start border-end digito'>{props.digito5}</div>
+				<div className='col rounded text-white text-center display-5 border-start border-end digito'> {props.digito4}</div>
+				<div className='col rounded text-white text-center display-5 border-start border-end digito'> {props.digito3}</div>
+				<div className='col rounded text-white text-center display-5 border-start border-end digito'> {props.digito2}</div>
+				<div className='col rounded text-white text-center display-5 border-start border-end digito'> {props.digito1}</div>
+
+			</div>
 		</div>
 	);
 };
